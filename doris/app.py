80f7b9f1ac7c3f3c8554e9003d881f40c3270001
Simon Ekström <simon.ekstrom@gmail.com>
2023-04-03 23:14:23 +0200

    stuff

--- doris/app.py
+++ doris/app.py
@@ -1,44 +1,89 @@
-from fastapi import FastAPI, Request
-from fastapi.responses import JSONResponse
-from sse_starlette.sse import EventSourceResponse
-from fastapi.middleware.cors import CORSMiddleware
+from datetime import datetime, timedelta
+import json
 
-import uvicorn
 import asyncio
+import jwt
+
+from fastapi import FastAPI, Depends, HTTPException
+from fastapi.security import OAuth2PasswordBearer
+from sse_starlette.sse import EventSourceResponse
 
 app = FastAPI()
-origins = [
-    "http://localhost:3000",
-]
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=origins,
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
-)
 
-STREAM_DELAY = 1  # second
-RETRY_TIMEOUT = 15000  # milisecond
+# origins = [
+#     "http://localhost:3000",
+# ]
+
+# app.add_middleware(
+#     CORSMiddleware,
+#     allow_origins=origins,
+#     allow_credentials=True,
+#     allow_methods=["*"],
+#     allow_headers=["*"],
+# )
+
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="/token")
+
+SECRET_KEY = "some secret key here pls"
+
+users = [
+    ('simon', 'kaka')
+]
+
+def current_user(token: str = Depends(oauth2_scheme)) -> str:
+    print('Checking token')
+    try:
+        payload = jwt.decode(token, SECRET_KEY, algorithms=['HS256'])
+        username: str = payload.get('user', None)
+        if username is None:
+            raise HTTPException(
+                status_code=401,
+                detail='Invalid token'
+            )
+        return username
+    except jwt.PyJWTError:
+        raise HTTPException(
+            status_code=401,
+            detail='Invalid token'
+        )
+
+@app.post('/api/login')
+def login(payload: dict[str, str]):
+    for user in users:
+        if payload['username'] == user[0] and payload['password'] == user[1]:
+            print(f'User {payload["username"]} logged in')
+            token_data = {
+                'user': payload['username'],
+                'exp': datetime.utcnow() + timedelta(days=1)
+            }
+            token = jwt.encode(token_data, SECRET_KEY, algorithm='HS256')
+            return {
+                'token': token
+            }
+    raise HTTPException(
+        status_code=401,
+        detail='Incorrect username or password'
+    )
+
 
 @app.post('/api/message')
-async def message_stream(request: Request):
-    payload = await request.json()
+async def message(payload: dict[str, str], user: str = Depends(current_user)):
     async def event_generator():
         for c in payload['message']:
-            # If client closes connection, stop sending events
-            if await request.is_disconnected():
-                break
-
-            yield {
-                "retry": RETRY_TIMEOUT,
-                "data": f'{c}'
-            }
-
-            await asyncio.sleep(STREAM_DELAY)
-
+            yield json.dumps({'chunk': c})
+            await asyncio.sleep(1)
     return EventSourceResponse(event_generator())
 
 
-if __name__ == "__main__":
-    uvicorn.run("app:app", host="127.0.0.1", port=5000, log_level="info")
+if __name__ == '__main__':
+    import uvicorn
+    import sys
+    uvicorn.run(
+        'app:app',
+        host='127.0.0.1',
+        port=5000,
+        ssl_keyfile='key.pem',
+        ssl_certfile='cert.pem',
+        log_level='info',
+        reload='-d' in sys.argv
+    )
